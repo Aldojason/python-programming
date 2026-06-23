76ebfac673fbb8b7cffe9db7984cab5e2c68d068
Aldojason <aldojason07@gmail.com>
2026-06-23 11:40:54 +0530

    pattern sums

---
+++ pattern3.py
@@ -0,0 +1,8 @@
+n=4
+for i in range(1,n+1):
+    space=n-i
+    star="*"*i  #if space "* " is given  it get changed into triangle  *
+                                                                    # * *
+    print(" "*space+star)  
+
+    
---
+++ pattern6.py
@@ -0,0 +1,8 @@
+n=4
+m=5
+num=1
+for i in range(n):
+    for j in range(m):
+        print(num,end=" ")
+        num+=1
+    print()
